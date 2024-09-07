b99c47b45a0f1c43c00780fa3d6d37ca96a87d10
AndrewLG92 <luisandrewgarcia92@gmail.com>
2024-09-07 13:22:28 -0700

    Header Update plus more
    Updated Style of Header and Animation
    Updated Body Height to fit more items added.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,10 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function openNav() {
-    document.getElementById("mySidenav").style.width = "165px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+document.onscroll = function () {
+    console.log("Window.innerHeight: " + window.innerHeight);
+    console.log("Window.srollY: " + window.scrollY);
+    console.log("document.body.clientHeight: " + document.body.clientHeight);
+    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+        console.log(document.getElementById('arrow'));
+        document.getElementById('arrow').setAttribute('style', 'display:none !important');
+    } else {
+        document.getElementById('arrow').removeAttribute('style', 'display:none !important');
+    }
 }
